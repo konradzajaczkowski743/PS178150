c793092916999fc49b9b6670a2651cdaba9b34d0
Konrad <konrad_zajaczkowski2005@wp.pl>
2024-03-11 14:26:10 +0100

    z

--- lab3/Zad12/main.c
+++ lab3/Zad12/main.c
@@ -6,9 +6,7 @@ int calculateGeometricSequenceRecursively(int n, int d)
     if(n==1){
         return 1;
     }
-    else{
-        return d*calculateGeometricSequenceRecursively(n-1,d);
-    }
+    return d*calculateGeometricSequenceRecursively(n-1,d);
 }
 int main()
 {
